dc899050e93f9101ed6a8b1290a0e96ecd2016af
JingX <JingXu@Yanchus-MacBook-Pro-2.local>
2016-01-22 03:07:00 -0800

    hw1revise

--- HW1/mapper.py
+++ HW1/mapper.py
@@ -6,49 +6,27 @@ import sys
 import re
 import string
 
-
-spam_emails = 0
-total_emails = 0
-total_spam_words = 0
-total_ham_words = 0
 filename = sys.argv[1]
-findwords = sys.argv[2].split(' ')
-count_dictionary = {}
-count_dictionary['spam'] = {}
-count_dictionary['ham'] = {}  
+findwords = sys.argv[2]
 emails = open(filename, "r")
+print "FINDWORDS", findwords
+
 for line in emails.readlines():
+    email_id = line.split('\t')[0]
     line = line.translate(string.maketrans("",""), string.punctuation) #strip punctuation
-    email = re.split(r'\t+', line) 
+    email = re.split(r'\t+', line) #strip words that include any numbers
     if len(email) != 4: #skip over email data formatting errors          
         continue
-    total_emails+=1
-    content = email[0] + email[2] + email[3] #concatenate subject and body sections into one string
+    content = email[2] + email[3] #concatenate subject and body sections into one string
     content = re.sub(r'\w*\d\w*', '', content).strip() #strip all words that include a number as these words are unlikely to be predictive
     content = re.sub("\s\s+" , " ", content) #strip all extra whitespaces
     list_content = content.split(' ') #list of each word in line
     if int(email[1]) == 1: #check if the email is spam or not, count instances of word appearing in spam/not-spam emails and total emails
-        spam_emails+=1
-        for word in findwords:
-            word = word.lower()
-            for each in list_content:
-                if each.lower() == word:
-                    if word not in count_dictionary['spam']: count_dictionary['spam'][word] = 1   
-                    else: count_dictionary['spam'][word]+=1
-                total_spam_words+=1
+        print "SPAM_COUNT", len(list_content) #emit key of SPAM_COUNT along with a word count value for later calculation of total SPAM words
+        for word in list_content:
+            print email_id, word.lower(), "SPAM", list_content.count(word) #emit email_id key, word, class, and count
     else: 
-        for word in findwords:
-            word = word.lower()
-            for each in list_content:
-                if each.lower() == word:
-                    if word not in count_dictionary['ham']: count_dictionary['ham'][word] = 1   
-                    else: count_dictionary['ham'][word]+=1
-                total_ham_words+=1    
-    print content
+        print "HAM_COUNT", len(list_content) #emit key of HAM_COUNT along with a word count value for later calculation of total HAM words
+        for word in list_content:
+            print email_id, word.lower(), "HAM", list_content.count(word) #emit email_id key, word, class, and count
     
-print "spam_emails", spam_emails
-print "total_emails", total_emails
-print "total_spam_words", total_spam_words
-print "total_ham_words", total_ham_words
-print "word", sys.argv[2]
-print count_dictionary
