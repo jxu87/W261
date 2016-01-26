0321375c6e584cb9596d9b030b932cb1ba704241
JingX <JingXu@Yanchus-MacBook-Pro-2.local>
2016-01-26 06:16:27 -0800

    hw2

--- HW1/mapper.py
+++ HW1/mapper.py
@@ -1,32 +1,18 @@
 #!/usr/bin/python
 ## mapper.py
 ## Author: Jing Xu
-## Description: mapper code for HW1.4
+## Description: mapper code for HW1.2
 import sys
 import re
 import string
 
-filename = sys.argv[1]
-findwords = sys.argv[2]
-emails = open(filename, "r")
-print "FINDWORDS", findwords
+filename = sys.argv[1] #read in first argument as the emails to be parsed
+findwords = sys.argv[2] #read in second argument as the word to be counted
+print "FINDWORD", findwords
 
-for line in emails.readlines():
-    email_id = line.split('\t')[0]
-    line = line.translate(string.maketrans("",""), string.punctuation) #strip punctuation
-    email = re.split(r'\t+', line) #strip words that include any numbers
-    if len(email) != 4: #skip over email data formatting errors          
-        continue
-    content = email[2] + email[3] #concatenate subject and body sections into one string
-    content = re.sub(r'\w*\d\w*', '', content).strip() #strip all words that include a number as these words are unlikely to be predictive
-    content = re.sub("\s\s+" , " ", content) #strip all extra whitespaces
-    list_content = content.split(' ') #list of each word in line
-    if int(email[1]) == 1: #check if the email is spam or not, count instances of word appearing in spam/not-spam emails and total emails
-        print "SPAM_COUNT", len(list_content) #emit key of SPAM_COUNT along with a word count value for later calculation of total SPAM words
-        for word in list_content:
-            print email_id, word.lower(), "SPAM", list_content.count(word) #emit email_id key, word, class, and count
-    else: 
-        print "HAM_COUNT", len(list_content) #emit key of HAM_COUNT along with a word count value for later calculation of total HAM words
-        for word in list_content:
-            print email_id, word.lower(), "HAM", list_content.count(word) #emit email_id key, word, class, and count
-    
+with open (filename, "r") as emails:
+    for email in emails.readlines(): #read each line in enronemail file, each corresponding to a single email sent
+        words = email.translate(string.maketrans("",""), string.punctuation) #strip all punctuation
+        words = words.split() #convert line into list of words
+        for word in words:
+            print word, 1
--- HW1/reducer.py
+++ HW1/reducer.py
@@ -1,57 +1,21 @@
 #!/usr/bin/python
 ## reducer.py
 ## Author: Jing Xu
-## Description: reducer code for HW1.4
+## Description: reducer code for HW1.2
 
 import sys
-import re
-import string
-import ast
 
-spam_emails = 0
-ham_emails = 0
-total_spam_words = 0
-total_ham_words = 0
-findwords = ''
-filename = ''
+findword = None
 words = {} #creating unique word list
 
 for filenames in sys.argv[1:]: #open each filename in the countfile list
     myfile = open('%s'%filenames, "r")
     for line in myfile.readlines(): #read each line in mapper output
         line = line.split() #split each line into list
-        if line[0] == 'SPAM_COUNT': 
-            spam_emails+=1 #add 1 to spam_emails
-            total_spam_words+=int(line[1]) #add spam words in email to total_spam_words
-        elif line[0] == 'HAM_COUNT': 
-            ham_emails+=1 #add 1 to ham_emails
-            total_ham_words+=int(line[1]) #add ham words in email to total_spam_words
-        elif line[0] == 'FINDWORDS': findwords = line[1:] #store findword in memory
-        else: 
-            word = str(line[1])
-            if line[2] == "SPAM": #sort word into SPAM dictionary
-                if word not in words: #create new dictionary index for word if not already existing
-                    words[word] = {}
-                    words[word]['SPAM'] = int(line[3]) #set count of the number of word in spam emails to 1
-                else:
-                    if 'SPAM' in words[word]: words[word]['SPAM']+=int(line[3]) #add 1 to number of word in spam emails
-                    else: words[word]['SPAM'] = int(line[3]) #if word exists in dictionary but not the spam count, create spam count for the word
-            else: #sort word into HAM dictionary
-                if word not in words: #create new dictionary index for word if not already existing
-                    words[word] = {}
-                    words[word]['HAM'] = int(line[3]) #set count of the number of word in ham emails to 1            
-                else: 
-                    if 'HAM' in words[word]: words[word]['HAM']+=int(line[3]) #add 1 to number of word in ham emails
-                    else: words[word]['HAM'] = int(line[3]) #if word exists in dictionary but not the ham count, create spam count for the word
+        if line[0] == "FINDWORD": findword = line[1]
+        else:
+            for word in line:
+                if word not in words: words[word] = 1
+                else: words[word]+=1
 
-for findword in findwords:
-    prior_spam = float(spam_emails)/(float(spam_emails)+float(ham_emails)) #prior spam = spam emails / total emails
-    prior_ham = float(ham_emails)/(float(spam_emails)+float(ham_emails)) #prior ham = ham emails / total emails
-    try: spam_probability = float(words[findword]['SPAM'])/float(total_spam_words) #spam probability is the number of occurrences of word in spam emails / total words in spam emails
-    except: spam_probability = 'N/A'
-    try: ham_probability = float(words[findword]['HAM'])/float(total_ham_words) #ham probability is the number of occurrences of word in ham emails / total words in non-spam emails
-    except: ham_probability = 'N/A'
-    print "Class conditional: SPAM,", findword, spam_probability
-    print "Class conditional: HAM,", findword, ham_probability
-    print findword, "SPAM Prior =", prior_spam
-    print findword, "HAM Prior =", prior_ham
+print words[findword]
